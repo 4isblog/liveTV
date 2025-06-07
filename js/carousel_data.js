edaef5e7fd676bd02f02e82edecb59bf4ca3788d
Agnes <admin@4is.cc>
2025-06-08 02:02:46 +0800

    Update carousel_data.js

--- js/carousel_data.js
+++ js/carousel_data.js
@@ -2,7 +2,7 @@ window.CAROUSEL_IMAGES_DATA = [
     {
         "src": "https://tbz.ltyuanfang.cn/fj/2020/63116be532d47.jpg",
         "name": "图片一",
-        "link": "#"
+        "link": "https://www.baidu.com"
     },
     {
         "src": "https://api.seaya.link/thumbnails/1734784935222_yande.re%201191970%20pantyhose%20seifuku%20skirt_lift%20tagme.png",
