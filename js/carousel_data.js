a457c8e6745c48dd9f0471e5c6df1c783997f72c
Agnes <admin@4is.cc>
2025-06-08 01:59:20 +0800

    Add files via upload

---
+++ js/carousel_data.js
@@ -0,0 +1,27 @@
+window.CAROUSEL_IMAGES_DATA = [
+    {
+        "src": "https://api.seaya.link/thumbnails/1734784935222_yande.re%201191970%20pantyhose%20seifuku%20skirt_lift%20tagme.png",
+        "name": "图片一",
+        "link": "#"
+    },
+    {
+        "src": "https://api.seaya.link/thumbnails/1734784935222_yande.re%201191970%20pantyhose%20seifuku%20skirt_lift%20tagme.png",
+        "name": "图片二",
+        "link": "#"
+    },
+    {
+        "src": "https://api.seaya.link/thumbnails/1734784935222_yande.re%201191970%20pantyhose%20seifuku%20skirt_lift%20tagme.png",
+        "name": "图片三",
+        "link": "#"
+    },
+    {
+        "src": "https://api.seaya.link/thumbnails/1734784935222_yande.re%201191970%20pantyhose%20seifuku%20skirt_lift%20tagme.png",
+        "name": "图片四",
+        "link": "#"
+    },
+    {
+        "src": "https://api.seaya.link/thumbnails/1734784935222_yande.re%201191970%20pantyhose%20seifuku%20skirt_lift%20tagme.png",
+        "name": "图片五",
+        "link": "#"
+    }
+]; 
